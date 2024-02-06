840b0236a4be96716efc8d8482b7da008d9b7f90
Wow12322 <116585619+Wow12322@users.noreply.github.com>
2024-02-06 19:51:19 +0100

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeCreateComponent } from './recipe-create/recipe-create.component';
+import { IngredientsListComponent } from './ingredients-list/ingredients-list.component';
+import { CuisineListComponent } from './cuisine-list/cuisine-list.component';
+import { AboutComponent } from './about/about.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: 'recipes', component: RecipeListComponent },
+  { path: 'recipes/:id', component: RecipeDetailsComponent },
+  { path: 'recipes/:id/edit', component: RecipeEditComponent },
+  { path: 'recipes/create', component: RecipeCreateComponent },
+  { path: 'ingredients', component: IngredientsListComponent },
+  { path: 'cuisine', component: CuisineListComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'statistics', component: StatisticsComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
